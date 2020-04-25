a296d0f52586a5d4ebe4c4a2daf04142613204e1
Márton Józsa <morfyum@gmail.com>
2020-04-25 23:36:52 +0200

    First upload: maincss v1.0

---
+++ js/models.js
@@ -0,0 +1,22 @@
+// FONTOS! Külön <script> tagek között hívd meg a fájt és ezt a scripte.
+// Ugyan abban nem fog működni...
+
+function test01() {
+    alert("JS MODELS IS WORK :) ");
+}
+
+
+// SLIDESHOW
+// Meghívás a functiont: "carousel();" és opcionálisan a változót előtte: "var slideIndex = 0;"
+var slideIndex = 0;
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("image-slide");
+  for (i = 0; i < x.length; i++) {
+	x[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}    
+  x[slideIndex-1].style.display = "block";  
+  setTimeout(carousel, 5000); //1(000=1sec) Change image every 2 seconds
+}
