a14bd7a4a5f826bad856b9d092db25b479005639
Morfyum <morfyum@gmail.com>
2022-04-02 19:25:29 +0200

    Chang: 786p scrollbar from 0px to hidden. first plan of game-demo

---
+++ games/game-demo.js
@@ -0,0 +1,42 @@
+
+// Prevent Right Clicking -> Enable when cause an issue
+// Easier way inside .html file: <body oncontextmenu="return false;">
+//document.addEventListener('contextmenu', event => event.preventDefault());
+
+/*
+Lazy Loading: https://www.merixstudio.com/blog/lazy-loading-pure-javascript/
+*/
+
+
+/*
+// element which needs to enter full-screen mode
+var element = document.querySelector("#main-menu");
+
+// make the element go to full-screen mode
+element.requestFullscreen()
+	.then(function() {
+		// element has entered fullscreen mode successfully
+	})
+	.catch(function(error) {
+		// element could not enter fullscreen mode
+	});
+*/
+
+
+function settingsBar() {
+    document.getElementById("mainMenu").classList.toggle("show-game-menu");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.header-element')) {
+        var dropdowns = document.getElementsByClassName("maunMenu");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show-game-menu')) {
+                openDropdown.classList.remove('show-game-menu');
+            }
+        }
+    }
+}
