e286976e86d32a872791527c3fd64b5282d7f218
Morfyum <morfyum@gmail.com>
2023-02-19 17:34:08 +0100

    Nothing

--- js/models.js
+++ js/models.js
@@ -13,10 +13,11 @@ function carousel() {
   var i;
   var x = document.getElementsByClassName("image-slide");
   for (i = 0; i < x.length; i++) {
-	x[i].style.display = "none";  
+	  x[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}    
-  x[slideIndex-1].style.display = "block";  
+  x[slideIndex-1].style.display = "block";
   setTimeout(carousel, 5000); //1(000=1sec) Change image every 2 seconds
 }
+
